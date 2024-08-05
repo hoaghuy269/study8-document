d8843852d5df3b8503f2c64de03da87072926938
HuyNH <huynh@unit.com.vn>
2024-08-05 17:29:25 +0700

    [HuyNH] Create table system_otp

---
+++ 01.Database/01.Table/system_otp.sql
@@ -0,0 +1,20 @@
+/**
+ * system_otp
+ * @Date: 2024-08-05
+ * @Author: HuyNH
+ * @Desc: Mã OTP đã gửi cho user
+ */
+CREATE TABLE system_otp (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    otp_type SMALLINT,
+    otp_code VARCHAR(50),
+    active SMALLINT,
+    sent_date TIMESTAMP NOT NULL,
+    expiry_date TIMESTAMP NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    created_id BIGINT,
+    deleted SMALLINT,
+    deleted_date TIMESTAMP,
+    deleted_id BIGINT
+);
