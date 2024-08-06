a384ae27a952d7e0d7a443a4ced91f88bb17c902
HuyNH <huynh@unit.com.vn>
2024-08-06 15:35:42 +0700

    [HuyNH] Add Study8 Domain config

---
+++ 01.Database/02.Insert/[0005] system_config - STUDY8 Domain.sql
@@ -0,0 +1,9 @@
+/**
+ * system_config
+ * @Date: 2024-08-06
+ * @Author: HuyNH
+ * @Desc: Thêm config domain chính
+ */
+INSERT INTO system_config
+(group_code, code, value, created_date, created_id)
+VALUES('SYSTEM', 'STUDY8_DOMAIN', 'https://study8.com', CURRENT_TIMESTAMP, 1);
