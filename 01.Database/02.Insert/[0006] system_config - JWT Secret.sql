2640ac712eed3c58e8ac21440d607f280196651a
HuyNH <huynh@unit.com.vn>
2024-08-08 17:27:19 +0700

    [HuyNH] Config jwt secret

---
+++ 01.Database/02.Insert/[0006] system_config - JWT Secret.sql
@@ -0,0 +1,9 @@
+/**
+ * system_config
+ * @Date: 2024-08-06
+ * @Author: HuyNH
+ * @Desc: Config jwt secret
+ */
+INSERT INTO system_config
+(group_code, code, value, created_date, created_id)
+VALUES('SYSTEM', 'JWT_SECRET', 'b90e74f718b4d7093728c120572ce660b3368da6fcd91d48aea06e6859f060e3', CURRENT_TIMESTAMP, 1);
